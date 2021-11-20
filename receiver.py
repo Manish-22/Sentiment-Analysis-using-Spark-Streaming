e832305c4d56fbd85fbdcb1bfa02a5bb390284ca
manish-22 <manishreddym22@gmail.com>
2021-11-20 18:49:49 +0530

    dataframe

--- receiver.py
+++ receiver.py
@@ -6,14 +6,40 @@ from pyspark.streaming import StreamingContext
 from pyspark.sql import Row,SQLContext
 import sys
 import requests
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode
+from pyspark.sql.functions import split
 
-conf=SparkConf()
-conf.setAppName("BigData")
-sc=SparkContext.getOrCreate(conf=conf)
+spark=SparkSession.builder.appName("bIG DATA").getOrCreate()
 
-ssc=StreamingContext(sc,5)
+#Uncomment this for datastreaming
+"""
+# conf=SparkConf()
+# conf.setAppName("BigData")
+# sc=SparkContext.getOrCreate(conf=conf)
 
-dataStream=ssc.socketTextStream("localhost",6100)
-ssc.start()
-ssc.awaitTermination()
-ssc.stop()
+# ssc=StreamingContext(sc,5)
+
+# dataStream=ssc.socketTextStream("localhost",6100)
+# dataStream.
+# ssc.start()
+# ssc.awaitTermination()
+# ssc.stop()
+"""
+
+lines = spark.readStream.format("socket").option("host", "localhost").option("port", 6100).load()
+words = lines.select(
+   explode(
+       split(lines.value, " ")
+   ).alias("word")
+)
+wordCounts = words.groupBy("word").count()
+query = wordCounts \
+    .writeStream \
+    .outputMode("complete") \
+    .format("console") \
+    .start()
+
+query.awaitTermination()
+
+
